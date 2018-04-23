475ec90bf2793c0767d2130123317918496cdb68
Nattapat White <nxw13a@lib-301-09.local>
2018-04-23 15:46:20 -0500

    FINALLY PUSH

--- JSONBETTERXML/MasterViewController.swift
+++ JSONBETTERXML/MasterViewController.swift
@@ -1,95 +1,122 @@
 //
 //  MasterViewController.swift
-//  JSONBETTERXML
+//  swiftforandroid
 //
-//  Created by Nattapat White on 3/9/18.
-//  Copyright © 2018 Nattapat White. All rights reserved.
+//  Created by Darius Bell on 3/9/18.
+//  Copyright © 2018 Darius Bell. All rights reserved.
 //
-
 import UIKit
 
 class MasterViewController: UITableViewController {
-
+    
     var detailViewController: DetailViewController? = nil
-    var objects = [Any]()
-
-
+    var objects = [String]()
+    //the json file url
+    let URL_HEROES = "https://api.myjson.com/bins/70xp1"
+    //the label we create
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        navigationItem.leftBarButtonItem = editButtonItem
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        navigationItem.rightBarButtonItem = addButton
-        if let split = splitViewController {
-            let controllers = split.viewControllers
-            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
-        }
+        getJsonFromUrl();
+        //        // Do any additional setup after loading the view, typically from a nib.
+        //        navigationItem.leftBarButtonItem = editButtonItem
+        //
+        //        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+        //        navigationItem.rightBarButtonItem = addButton
+        //        if let split = splitViewController {
+        //            let controllers = split.viewControllers
+        //            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        //        }
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @objc
     func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
+        objects.insert(String(), at: 0)
         let indexPath = IndexPath(row: 0, section: 0)
         tableView.insertRows(at: [indexPath], with: .automatic)
     }
-
+    
     // MARK: - Segues
-
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let object = objects[indexPath.row]
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                controller.detailItem = object;
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
         }
     }
-
+    
     // MARK: - Table View
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return objects.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        let object = objects[indexPath.row] as! NSDate
+        
+        let object = objects[indexPath.row]
         cell.textLabel!.text = object.description
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
+    
+    func getJsonFromUrl(){
+        //creating a NSURL
+        let url = NSURL(string: URL_HEROES)
+        
+        //fetching the data from the url
+        URLSession.shared.dataTask(with: (url as URL?)!, completionHandler: {(data, response, error) -> Void in
+            
+            if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary {
+                
+                //printing the json in console
+                print(jsonObj!.value(forKey: "stuff")!)
+                
+                //getting the avengers tag array from json and converting it to NSArray
+                if let heroeArray = jsonObj!.value(forKey: "stuff") as? NSArray {
+                    //looping through all the elements
+                    for name in heroeArray{
+                        //adding the name to the array
+                        self.objects.append((name as? String)!)
+                    }
+                    
+                }
+            }
+        }).resume()
     }
-
-
+    
+    
+    //    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    //        if editingStyle == .delete {
+    //            objects.remove(at: indexPath.row)
+    //            tableView.deleteRows(at: [indexPath], with: .fade)
+    //        } else if editingStyle == .insert {
+    //            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    //        }
+    //    }
+    
+    
+    
+    
 }
-
